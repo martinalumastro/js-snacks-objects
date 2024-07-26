a0543b439b17c3ff3d703ff913c2600c48ed3406
Martina Lumastro <martinalumastro99@gmail.com>
2024-07-26 13:53:47 +0200

    correzione snack 2

--- js/snack-02.js
+++ js/snack-02.js
@@ -51,17 +51,61 @@ let people = [
 let etaPerGuidare = []
 
 
+// for(i = 0; i < people.length; i++) {
+//     personaCorrente = people[i]
+
+//     personaCorrente.eta = parseInt(personaCorrente.eta)
+//     //console.log(personaCorrente.eta)
+
+//     if(personaCorrente.eta >= 18) {
+//         console.log(etaPerGuidare.push(personaCorrente) + personaCorrente.nome + ' ' + personaCorrente.cognome + ' puoi guidare')
+//     } else {
+//         console.log(etaPerGuidare.push(personaCorrente) + personaCorrente.nome + ' ' + personaCorrente.cognome + ' non puoi guidare')
+//     }
+
+//     console.log(etaPerGuidare)
+// }
+
 for(i = 0; i < people.length; i++) {
     personaCorrente = people[i]
 
-    personaCorrente.eta = parseInt(personaCorrente.eta)
-    //console.log(personaCorrente.eta)
+    console.log(personaCorrente)
+    // nome + cognoe + ? + può guidere
+    let message = personaCorrente.nome + ' ' + personaCorrente.cognome
 
-    if(personaCorrente.eta >= 18) {
-        console.log(etaPerGuidare.push(personaCorrente) + personaCorrente.nome + ' ' + personaCorrente.cognome + ' puoi guidare')
-    } else {
-        console.log(etaPerGuidare.push(personaCorrente) + personaCorrente.nome + ' ' + personaCorrente.cognome + ' non puoi guidare')
+    if(!canDrive(personaCorrente)) {
+        message += ' non'
     }
 
-    console.log(etaPerGuidare)
+    message += ' può guidare'
+
+    etaPerGuidare.push(message)
 }
+
+console.log(etaPerGuidare)
+
+// //function
+function canDrive(people) {
+
+//     const age = person.eta
+//     //console.log(age >= 18) //boolean
+
+//     // if(age >= 18) {
+//     //     //return true se può guidere
+//     //     return true
+//     // } else {
+//     //     //return false se non può guidere
+//     //     return false
+//     // }
+
+//     // ABBREVIATO
+    return people.eta >= 18
+}
+
+// const res = canDrive({
+//     nome: 'Giulio',
+//     cognome: 'Bianchi',
+//     eta: '13',
+// })
+
+// console.log(res)
